5e76226af1d47bd4d67fb47fbb3423d74d192ffe
unknown <lucasacioffi@gmail.com>
2021-01-17 21:01:39 -0300

    sistema de detecção de tweets duplicados

--- ColetorTwitter.py
+++ ColetorTwitter.py
@@ -1,6 +1,11 @@
 import twint
 import json
 import splunklib.client as client
+import sqlite3 as sl
+
+tweetsParaBuscar = 10
+splunkUsername = "admin"
+splunkPassword = "admin123"
 
 def printResults(resultados):
 	for result in resultados:
@@ -25,37 +30,91 @@ def createTweetJson(tweet):
 	return tweetJson
 	
 def openSplunkConnection():	
-	service = client.connect(host='localhost',port=8089,username='admin',password='admin123')
+	service = client.connect(host='localhost',port=8089,username=splunkUsername,password=splunkPassword)
 	indexConection = service.indexes["main"] 	
 	return indexConection
 	
 def sendJsonToSplunk(jsonData, indexConection): 
 	indexConection.submit(jsonData)
+	
+def verificarTweetJaVisto(tweetId):
+	with dbConnection:
+		data = dbConnection.execute("SELECT COUNT(*) FROM TWEETS WHERE tweetid = " + str(tweetId))
+		for row in data:
+			if (row[0] == 0):
+				return False
+			else:
+				return True
+				
+def adicionarATweetsJavistos(tweet):
+	try:
+		with dbConnection:
+			dbConnection.execute(" INSERT INTO TWEETS VALUES (" + str(tweet.id) + ")")
+	except:
+		pass
+
+def removerDuplicados(resultados):
+	resultadosNovos = []
+	for result in resultados:
+		tweetid = result.id
+		if(not verificarTweetJaVisto(tweetid)):
+			resultadosNovos.append(result)
+	return resultadosNovos
+	
+def procurarTweets(termo):
+	resultados = []
+
+	# Configure
+	c = twint.Config()
+	c.Store_object = True
+	c.Store_object_tweets_list = resultados
+	c.Hide_output = True
+	c.Limit = tweetsParaBuscar
+	c.Store_json = True
+	c.Filter_retweets = False
+	c.Profile_fulle = True
+
+	#Search params
+	c.All = termo
+
+	# Run
+	twint.run.Search(c)
+	print(termo + " - " + str(len(resultados)) + " tweets coletados")
+	
+	return resultados
 
 #################### MAIN ####################
 	
-resultados = []
+#Inicializar banco de dados que armazena tweets duplicados:
+dbConnection = sl.connect('dados.db')
+try:
+	with dbConnection:
+		dbConnection.execute("""
+			CREATE TABLE TWEETS (
+				tweetid INTEGER NOT NULL PRIMARY KEY
+			); 
+		""")
+except:
+	print("Banco de dados ja existe")
+	
+resultadosTotal = []
+resultadosTotal = resultadosTotal + procurarTweets("santander")
+resultadosFinal = removerDuplicados(resultadosTotal)
 
-# Configure
-c = twint.Config()
-c.Store_object = True
-c.Store_object_tweets_list = resultados
-c.Hide_output = True
-c.Limit = 10
+if (len(resultadosFinal) > 0):
 
-#Search params
-c.Username = "santander"
+	#Open splunk connection
+	indexConection = openSplunkConnection()
+	for result in resultadosFinal:
+		resultJson = createTweetJson(result)
+		print(resultJson)
+		print("\n")
+		sendJsonToSplunk(resultJson, indexConection)
+		adicionarATweetsJavistos(result)
+	print(str(len(resultadosFinal)) + " tweets coletados")
 
-#Open splunk connection
-indexConection = openSplunkConnection()
+else:
+	print("Nenhum tweet novo encontrado")
 
-# Run
-twint.run.Search(c)
-for result in resultados:
-	resultJson = createTweetJson(result)
-	print(resultJson)
-	print("\n")
-	sendJsonToSplunk(resultJson, indexConection)
 
-print(str(len(resultados)) + " tweets coletados")
 
